04dc20bcea7a886773a2832cf171acac15a75a93
Sampsa Kares <sampsa.kares@pp.inet.fi>
2018-01-10 02:17:32 +0200

    maybe this works after i sleep

--- src/App.js
+++ src/App.js
@@ -8,11 +8,12 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">WoW Classic</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-          asd
+          <h2>Choose Class</h2>
+          <div id="classes"></div>
+          <ClassIcon className="warrior" />
         </p>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -6,3 +6,31 @@ import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
+var allClasses = ["warrior", "paladin", "shaman", "hunter", "druid", "rogue", "warlock", "mage", "priest"];
+
+var lol = "<span>LOL</span>"
+
+for (let i = 0; i < allClasses.length; i++){
+  var value = allClasses[i];
+  document.getElementById('classes').innerHTML +=  "<img src='img/"+value+".png' alt="+value+" id="+value+" height='75' onclick='chooseClass("+value+")' />";
+}
+
+function ClassIcon(props) {
+  const { className } = props;
+
+  return (
+      <Image src={'/img/${className}.png'} />
+  )
+}
+
+function chooseClass(className){
+
+}
+
+// function chooseClass(className){
+//   for (let i = 0; i < allClasses.length; i++){
+//       document.getElementById(allClasses[i]).classList.remove("greyOut");
+//   }
+//   document.getElementById(className).classList.add("greyOut");
+// }
