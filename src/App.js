c8a5ac276ca6bc6a3d8e51b90638c4e65d4459fd
Sampsa Kares <sampsa.kares@pp.inet.fi>
2018-01-10 19:52:39 +0200

    .map function

--- src/App.js
+++ src/App.js
@@ -5,13 +5,14 @@ import ClassIcon from './ClassIcon.js';
 import InfoPage from './InfoPage.js';
 
 class App extends Component {
-  state = {selectedClass: ""};
+  state = {selectedClass: "warrior"};
 
   handleClick = (name) => {
     this.setState({selectedClass: name});
   };
 
   render() {
+    const allClasses = ["warrior", "paladin", "shaman", "hunter", "druid", "rogue", "warlock", "mage", "priest"];
     return (
       <div className="App">
         <header className="App-header">
@@ -19,17 +20,11 @@ class App extends Component {
           <h1 className="App-title">WoW Classic</h1>
         </header>
         <p className="App-intro">Choose Class</p>
-          <ClassIcon name="warrior" handleClick={this.handleClick} active={this.state.selectedClass === "warrior"} />
-          <ClassIcon name="paladin" handleClick={this.handleClick} active={this.state.selectedClass === "paladin"} />
-          <ClassIcon name="shaman" handleClick={this.handleClick} active={this.state.selectedClass === "shaman"} />
-          <ClassIcon name="hunter" handleClick={this.handleClick} active={this.state.selectedClass === "hunter"} />
-          <ClassIcon name="druid" handleClick={this.handleClick} active={this.state.selectedClass === "druid"} />
-          <ClassIcon name="rogue" handleClick={this.handleClick} active={this.state.selectedClass === "rogue"} />
-          <ClassIcon name="warlock" handleClick={this.handleClick} active={this.state.selectedClass === "warlock"} />
-          <ClassIcon name="mage" handleClick={this.handleClick} active={this.state.selectedClass === "mage"} />
-          <ClassIcon name="priest" handleClick={this.handleClick} active={this.state.selectedClass === "priest"} />
-          <div/>
-          <InfoPage id="infoPage" />
+            { allClasses.map(name => {
+            return <ClassIcon key={name} name={name} handleClick={this.handleClick} active={this.state.selectedClass === name} />
+            }) }
+        <div/>
+        <InfoPage id="infoPage" />
       </div>
     );
   }
--- src/ClassIcon.js
+++ src/ClassIcon.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 import InfoPage from './InfoPage.js';
 
-//var allClasses = ["warrior", "paladin", "shaman", "hunter", "druid", "rogue", "warlock", "mage", "priest"];
+// const allClasses = ["warrior", "paladin", "shaman", "hunter", "druid", "rogue", "warlock", "mage", "priest"];
 
 class ClassIcon extends Component {
 
     render() {
         // name=warrior, handleClick=handleClick(), active=true/false
         const { name, handleClick, active } = this.props;
-        //classicon gets classname with "" or "greyOut"
         const className = active ? "" : "greyOut";
+
         return (
         <img name={name} src={`/img/`+name+`.png`} alt={name} onClick={() => handleClick(name)} className={className} height='70' />
         );
